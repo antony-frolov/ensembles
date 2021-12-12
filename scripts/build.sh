eeede269b93f2d04e3edf67da5c50bf36a722406
antony-frolov <i.am.antonyfrolov@gmail.com>
2021-12-12 12:48:48 +0300

    Add --no-cache flag to build.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker build . -t ml_server
+docker build . -t ml_server --no-cache
--- scripts/run.sh
+++ scripts/run.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-docker run -i -t --rm -p 5000:5000 ml_server 
+docker run -i -t --rm -p 5000:5000 ml_server
